70f770806107608ad73606e844f5aa646a66d8a0
Hoppy0616 <hodahoda.20430@gmail.com>
2021-08-26 18:13:52 +0900

    AddButtonの整備

--- components/Atom/AddButton.vue
+++ components/Atom/AddButton.vue
@@ -1,9 +1,24 @@
 <template>
-  
+  <button class="bg-gray-700 text-white py-2 px-4 rounded" :style="style">
+    {{ text }}
+  </button>
 </template>
 <script lang="ts">
   import Vue from 'vue'
   export default Vue.extend({
-  
+    props: {
+      text: {
+        required: true,
+        type: String,
+      },
+      width: {
+        default: 'auto',
+        type: String,
+      },
+      height: {
+        default: 'auto',
+        type: String,
+      },
+    }
 })
 </script>
